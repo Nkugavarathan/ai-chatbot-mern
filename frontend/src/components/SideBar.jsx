e60fec6d31c6295c4c58582d116d329ed7dbbfbd
Nkugavarathan <kugavarathan28@gmail.com>
2025-10-30 06:47:47 +0530

    appcontext created

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,6 +1,8 @@
 import React from "react"
+import { useAppContext } from "../context/AppContext"
 
 function SideBar() {
+  const { chats, selectedChat, theme, setTheme, user } = useAppContext()
   return <div>SideBar</div>
 }
 
