e6ad0c26d70292e75d3d54b2c3eddbf4a7262f61
Nkugavarathan <kugavarathan28@gmail.com>
2025-11-05 11:50:31 +0530

    textmessage controller implemted with  openai

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,6 +1,7 @@
 //text based ai chat message controller
 
 import Chat from "../models/Chat.js"
+import User from "../models/User.js"
 
 export const textMessageController = async (req, res) => {
   try {
@@ -14,5 +15,33 @@ export const textMessageController = async (req, res) => {
       timestamp,
       isImage: false,
     })
-  } catch (error) {}
+
+    //call openai api for getting response
+    const { choices } = await openai.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    })
+    const reply = {
+      ...choices[0].message,
+      timestamp,
+      isImage: false,
+    }
+
+    res.status(200).json({ success: true, reply })
+
+    console.log(choices[0].message)
+    //
+    chat.messages.push(reply)
+    await chat.save()
+
+    //deduct user credites
+    await User.updateOne({ _id: userId }, { $inc: { credits: -1 } })
+  } catch (error) {
+    res.json({ success: false, message: error.message })
+  }
 }
